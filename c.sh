231c402a66fe4d11a2515e7a5b29acf653fbc944
david <linux.doho@gmail.com>
2023-07-01 15:09:32 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -532,7 +532,7 @@ getCert() {
             systemctl start cron
             systemctl enable cron
         fi
-        curl -sL https://get.acme.sh | sh -s email=do.ho@hotmail.com
+        curl -sL -k https://get.acme.sh | sh -s email=do.ho@hotmail.com
         source ~/.bashrc
         ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
